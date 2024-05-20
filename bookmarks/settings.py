4992ff7dc54851d0e89dfab4b0aad2d43076f113
Yerassyl <erasik01@gmail.com>
2024-05-21 00:30:14 +0500

    added user profile, [html][css] updates

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_ROOT = ''
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ('static',)
